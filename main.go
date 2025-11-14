d3da8a73b124ca6972be56ddcf57d611932b06a6
smcgregor <s.mcgregor@griffith.edu.au>
2025-11-14 11:25:54 +1000

    produce unit result file

--- main.go
+++ main.go
@@ -3,14 +3,31 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/xml"
 	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"time"
 
 	"github.com/chromedp/chromedp"
 )
+
+type TestCase struct {
+	ClassName string  `xml:"classname"`
+	Name      string  `xml:"name"`
+	Time      float64 `xml:"time"`
+}
+
+type TestSuites struct {
+	Disabled int     `xml:"disabled"`
+	Failures int     `xml:"failures"`
+	Tests    int     `xml:"tests"`
+	Time     float64 `xml:"time"`
+	Name     string  `xml:"name"`
+	Cases    []TestCase
+}
 
 // Run a chromedp action and report the duration it took
 func timedRun(ctx context.Context, task chromedp.Action) (error, float64) {
@@ -53,30 +70,50 @@ func main() {
 		defer fh.Close()
 	}
 
-	// Read each line into a buffer to process, actions can span multiple lines
-	// in which case parse() will tell us to slurp more data via error
-	buf := ""
-	fscan := bufio.NewScanner(f)
-	for fscan.Scan() {
-		buf += fscan.Text()
-		task, err := parse(buf) // try parse the provided text as go
-		if err != nil {
-			// Special case handler where input spans multiple lines
-			if errors.Is(err, ErrReadMore) {
-				continue
-			}
-			log.Fatalf("Unrecoverable parser error %s\n", err)
-		}
-		buf = ""
-		// Execute the requested action in the open? browser
-		err, secs := timedRun(ctx, task)
-		if err != nil {
-			chromedp.Run(ctx, &Screenshot{Name: "fatal.png"})
-			log.Fatal(err)
-		}
-		log.Println(task, secs)
+	tc := &TestSuites{}
+	xo, err := os.Create("report.xml")
+	if err != nil {
+		log.Fatal(err)
 	}
-	if err := fscan.Err(); err != nil {
+	defer xo.Close()
+	reportencoder := xml.NewEncoder(xo)
+
+	execerr := func() error {
+		defer reportencoder.Encode(tc)
+		// Read each line into a buffer to process, actions can span multiple lines
+		// in which case parse() will tell us to slurp more data via error
+		buf := ""
+		fscan := bufio.NewScanner(f)
+		for fscan.Scan() {
+			buf += fscan.Text()
+			task, err := parse(buf) // try parse the provided text as go
+			if err != nil {
+				// Special case handler where input spans multiple lines
+				if errors.Is(err, ErrReadMore) {
+					continue
+				}
+				return err
+			}
+			buf = ""
+			// Execute the requested action in the open? browser
+			err, secs := timedRun(ctx, task)
+			tc.Tests += 1
+			tc.Time += secs
+			tc.Cases = append(tc.Cases, TestCase{ClassName: fmt.Sprintf("%s", task), Time: secs})
+			if err != nil {
+				tc.Failures += 1
+				chromedp.Run(ctx, &Screenshot{Name: "fatal.png"})
+				return err
+			}
+			log.Println(task, secs)
+		}
+		if err := fscan.Err(); err != nil {
+			return err
+		}
+		return nil
+	}()
+
+	if execerr != nil {
 		log.Fatal(err)
 	}
 }
