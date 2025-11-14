d662ba44255f0f526ed1760629c1aebf4f9c9fa1
smcgregor <s.mcgregor@griffith.edu.au>
2025-11-14 15:17:16 +1000

    better unit output

--- input.go
+++ input.go
@@ -23,47 +23,48 @@ func iFieldByName(v reflect.Value, name string) reflect.Value {
 }
 
 // Parse the provided line(s) as Go code
-func parse(expr string) (chromedp.Action, error) {
+func parse(expr string) (chromedp.Action, string, error) {
 	e, err := parser.ParseExpr(expr)
 	if err != nil {
 		// If we got an EOF, try getting more input
 		errs, ok := err.(scanner.ErrorList)
 		if ok && errs.Len() == 1 && strings.HasSuffix(errs[0].Msg, "found 'EOF'") {
-			return nil, ErrReadMore
+			return nil, "", ErrReadMore
 		}
-		return nil, err
+		return nil, "", err
 	}
 	// We're expecting thing{args:blah}
 	x, ok := e.(*ast.CompositeLit)
 	if !ok {
-		return nil, fmt.Errorf("Invalid definition found, expected thing{args:value}")
+		return nil, "", fmt.Errorf("Invalid definition found, expected thing{args:value}")
 	}
+	typename := x.Type.(*ast.Ident).Name
 	// Check we have a think - defaults to noop
-	act := resolveAction(x.Type.(*ast.Ident).Name)
+	act := resolveAction(typename)
 	actref := reflect.ValueOf(act).Elem()
 	// Walk the key:value pairs provided in the struct definition
 	for _, v := range x.Elts {
 		f, ok := v.(*ast.KeyValueExpr)
 		if !ok {
-			return nil, fmt.Errorf("No arguements given to type %q", x.Type)
+			return nil, "", fmt.Errorf("No arguements given to type %q", x.Type)
 		}
 		// Turn out the key/value into usable types
 		k := f.Key.(*ast.Ident)
 		v := f.Value.(*ast.BasicLit)
 		if v.Kind != token.STRING {
-			return nil, fmt.Errorf("Attribute %q on type %q is not a string", k, x.Type)
+			return nil, "", fmt.Errorf("Attribute %q on type %q is not a string", k, x.Type)
 		}
 		// Strings are always quoted, unquote for the raw value
 		s, err := strconv.Unquote(v.Value)
 		if err != nil {
-			return nil, err
+			return nil, "", err
 		}
 		// Set the k:v attributes on our action type
 		fnc := iFieldByName(actref, k.Name)
 		if !fnc.IsValid() {
-			return nil, fmt.Errorf("Invalid field given for %q %q=%q", act, k, v)
+			return nil, "", fmt.Errorf("Invalid field given for %q %q=%q", act, k, v)
 		}
 		fnc.SetString(s)
 	}
-	return act, nil
+	return act, typename, nil
 }
--- main.go
+++ main.go
@@ -15,18 +15,49 @@ import (
 )
 
 type TestCase struct {
-	ClassName string  `xml:"classname,attr"`
-	Name      string  `xml:"name,attr"`
-	Time      float64 `xml:"time,attr"`
+	XMLName   xml.Name `xml:"testcase"`
+	ClassName string   `xml:"classname,attr"`
+	Name      string   `xml:"name,attr"`
+	Time      float64  `xml:"time,attr"`
 }
 
-type TestSuites struct {
+type TestSuite struct {
+	XMLName  xml.Name   `xml:"testsuite"`
 	Disabled int        `xml:"disabled,attr"`
 	Failures int        `xml:"failures,attr"`
 	Tests    int        `xml:"tests,attr"`
 	Time     float64    `xml:"time,attr"`
 	Name     string     `xml:"name,attr"`
 	Cases    []TestCase `xml:"testcase"`
+}
+
+type TestSuites struct {
+	XMLName  xml.Name    `xml:"testsuites"`
+	Disabled int         `xml:"disabled,attr"`
+	Failures int         `xml:"failures,attr"`
+	Tests    int         `xml:"tests,attr"`
+	Time     float64     `xml:"time,attr"`
+	Name     string      `xml:"name,attr"`
+	Suites   []TestSuite `xml:"testsuite"`
+}
+
+func writeSuite(ts *TestSuite) error {
+	tc := &TestSuites{
+		Name:   ts.Name,
+		Suites: []TestSuite{*ts},
+	}
+	tc.Failures = ts.Failures
+	tc.Tests = ts.Tests
+	tc.Time = ts.Time
+	xo, err := os.Create("report.xml")
+	if err != nil {
+		return err
+	}
+	defer xo.Close()
+	xo.Write([]byte(xml.Header))
+	reportencoder := xml.NewEncoder(xo)
+	reportencoder.Encode(tc)
+	return nil
 }
 
 // Run a chromedp action and report the duration it took
@@ -70,24 +101,20 @@ func main() {
 		defer fh.Close()
 	}
 
-	tc := &TestSuites{}
-	xo, err := os.Create("report.xml")
-	if err != nil {
-		log.Fatal(err)
+	tcs := &TestSuite{
+		Name:  *cliInput,
+		Cases: []TestCase{},
 	}
-	defer xo.Close()
-	xo.Write([]byte(xml.Header))
-	reportencoder := xml.NewEncoder(xo)
+	defer writeSuite(tcs)
 
 	execerr := func() error {
-		defer reportencoder.Encode(tc)
 		// Read each line into a buffer to process, actions can span multiple lines
 		// in which case parse() will tell us to slurp more data via error
 		buf := ""
 		fscan := bufio.NewScanner(f)
 		for fscan.Scan() {
 			buf += fscan.Text()
-			task, err := parse(buf) // try parse the provided text as go
+			task, name, err := parse(buf) // try parse the provided text as go
 			if err != nil {
 				// Special case handler where input spans multiple lines
 				if errors.Is(err, ErrReadMore) {
@@ -98,11 +125,11 @@ func main() {
 			buf = ""
 			// Execute the requested action in the open? browser
 			err, secs := timedRun(ctx, task)
-			tc.Tests += 1
-			tc.Time += secs
-			tc.Cases = append(tc.Cases, TestCase{ClassName: fmt.Sprintf("%s", task), Time: secs})
+			tcs.Tests += 1
+			tcs.Time += secs
+			tcs.Cases = append(tcs.Cases, TestCase{ClassName: name, Name: fmt.Sprintf("%s", task), Time: secs})
 			if err != nil {
-				tc.Failures += 1
+				tcs.Failures += 1
 				chromedp.Run(ctx, &Screenshot{Name: "fatal.png"})
 				return err
 			}
@@ -115,6 +142,6 @@ func main() {
 	}()
 
 	if execerr != nil {
-		log.Fatal(err)
+		log.Fatal(execerr)
 	}
 }
